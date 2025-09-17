e30c7fe1ea581c7ddf22022ad8b1443d84b18eb7
Alclides <alclidessantos010@gmail.com>
2025-09-17 15:52:49 -0300

    criando classe livro

---
+++ src/Livro.java
@@ -0,0 +1,42 @@
+public class Livro {
+    private String nome;
+    private String autor;
+    private Boolean estaDisponivel = false;
+
+
+    //construtor
+    public Livro(String nome, String autor) {
+        this.nome = nome;
+        this.autor = autor;
+    }
+
+    public Livro (){
+
+    }
+
+    public void marcarEmprestado() {
+        this.estaDisponivel = false;
+    }
+
+    public void marcarDisponivel() {
+        this.estaDisponivel = true;
+    }
+
+    public boolean estaDisponivel()
+    {
+        return this.estaDisponivel;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro: " + this.nome + " author é: " + this.autor;
+    }
+}
